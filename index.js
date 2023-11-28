fc092ba5b48e4429b74e6c0eb7608b2d5b4c0d50
Mathis Santos <mail@mathissantos.com>
2023-11-28 19:08:52 +0100

    added assets

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 // 4. another variable stands for the reset state of the button
 
 const bodyElement = document.querySelector('[data-js="body"]');
+const cardElement = document.querySelector('[data-js="card]');
+
 const toggleButton = document.querySelector('[data-js="toggleButton"]');
 
 toggleButton.addEventListener("click", () => {
